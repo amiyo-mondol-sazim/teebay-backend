009fbab194f4f93c6cf7954fc83aaaf98c559e64
amiyo-sazim <amiyo.mondol@sazim.io>
2026-02-13 00:55:11 +0600

    Refactor: remove pg_cron dependency from migration

--- src/db/migrations/Migration20260211000000_add_rental_expiry_cron_job.ts
+++ src/db/migrations/Migration20260211000000_add_rental_expiry_cron_job.ts
@@ -2,10 +2,6 @@ import { Migration } from "@mikro-orm/migrations";
 
 export class Migration20260211000000_add_rental_expiry_cron_job extends Migration {
   async up(): Promise<void> {
-    // Enable pg_cron extension
-    this.addSql('create extension if not exists pg_cron;');
-
-    // Create function to mark expired rentals as available
     this.addSql(`
       create or replace function mark_expired_rentals_as_available()
       returns void as $$
@@ -22,22 +18,9 @@ export class Migration20260211000000_add_rental_expiry_cron_job extends Migratio
       end;
       $$ language plpgsql;
     `);
-
-    // Schedule the cron job to run daily at midnight
-    this.addSql(`
-      select cron.schedule(
-        'mark-expired-rentals-available',
-        '0 0 * * *',
-        $$select mark_expired_rentals_as_available()$$
-      );
-    `);
   }
 
   async down(): Promise<void> {
-    // Unschedule the cron job
-    this.addSql("select cron.unschedule('mark-expired-rentals-available');");
-
-    // Drop the function
     this.addSql('drop function if exists mark_expired_rentals_as_available();');
   }
 }
