758db04aaa969d6fcf714e282bb84015433f21f3
amiyo-sazim <amiyo.mondol@sazim.io>
2026-02-15 16:49:29 +0600

    fix(notifications): add type transformation for query parameters

--- src/modules/notifications/notifications.dtos.ts
+++ src/modules/notifications/notifications.dtos.ts
@@ -1,16 +1,19 @@
 import { ApiProperty } from "@nestjs/swagger";
 
+import { Type } from "class-transformer";
 import { IsInt, IsOptional, Max, Min } from "class-validator";
 
 export class GetNotificationsQueryDto {
   @ApiProperty({ required: false, default: 1 })
   @IsOptional()
+  @Type(() => Number)
   @IsInt()
   @Min(1)
   page?: number;
 
   @ApiProperty({ required: false, default: 20 })
   @IsOptional()
+  @Type(() => Number)
   @IsInt()
   @Min(1)
   @Max(100)
